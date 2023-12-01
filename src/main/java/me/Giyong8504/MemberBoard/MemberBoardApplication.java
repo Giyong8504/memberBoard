2d8b5718324fc38c1f76c404d2a0a0a9a13f5729
Giyong8504 <kky5163@naver.com>
2023-12-01 23:55:05 +0900

    초기 설정

---
+++ src/main/java/me/Giyong8504/MemberBoard/MemberBoardApplication.java
@@ -0,0 +1,13 @@
+package me.Giyong8504.MemberBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/Giyong8504/MemberBoard/MemberBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Giyong8504.MemberBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
