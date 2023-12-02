8b4493360a5452e7b6f2b02ea76cbb15fd4d3ee0
Giyong8504 <kky5163@naver.com>
2023-12-03 01:50:47 +0900

    data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO board_data (title, content, author, reg_dt, mod_dt) VALUES ('제목1', '내용1', 'user1', NOW(), NOW())
+INSERT INTO board_data (title, content, author, reg_dt, mod_dt) VALUES ('제목2', '내용2', 'user2', NOW(), NOW())
+INSERT INTO board_data (title, content, author, reg_dt, mod_dt) VALUES ('제목3', '내용3', 'user3', NOW(), NOW())
