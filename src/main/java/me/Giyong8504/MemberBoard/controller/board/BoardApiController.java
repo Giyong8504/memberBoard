2e44c66f855da6b4971566cf2de11b819d5e1451
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2023-12-04 16:15:54 +0900

    boards -> board로 변경

--- src/main/java/me/Giyong8504/MemberBoard/controller/board/BoardApiController.java
+++ src/main/java/me/Giyong8504/MemberBoard/controller/board/BoardApiController.java
@@ -1,4 +1,4 @@
-package me.Giyong8504.MemberBoard.controller;
+package me.Giyong8504.MemberBoard.controller.board;
 
 import lombok.RequiredArgsConstructor;
 import me.Giyong8504.MemberBoard.dto.AddBoardDataRequest;
@@ -19,7 +19,7 @@ public class BoardApiController {
     private final BoardService boardService;
 
     // HTTP 메서드가 POST 일 때 전달받은 URL과 동일하면 메서드로 매핑.
-    @PostMapping("/api/boards")
+    @PostMapping("/api/board")
     public ResponseEntity<BoardData> addBoardData(@RequestBody AddBoardDataRequest request) {
         BoardData saveBoardData = boardService.save(request);
 
@@ -27,16 +27,16 @@ public class BoardApiController {
     }
 
     // GET 요청이 들어오면 BoardDataResponse로 파싱하여 클라이언트에 반환.
-    @GetMapping("/api/boards")
+    @GetMapping("/api/board")
     public ResponseEntity<List<BoardDataResponse>> findAllBoardData() {
-        List<BoardDataResponse> boards = boardService.findAll()
+        List<BoardDataResponse> board = boardService.findAll()
                 .stream().map(BoardDataResponse::new).toList();
 
-        return ResponseEntity.ok().body(boards);
+        return ResponseEntity.ok().body(board);
     }
 
     // {id}에 값이 들어오면 findBoard()로 넘어가 {id} 글을 찾고 게시글 정보를 body에 담아 웹브라우저에 보낸다.
-    @GetMapping("/api/boards/{id}")
+    @GetMapping("/api/board/{id}")
     public ResponseEntity<BoardDataResponse> findBoard(@PathVariable Long id) {
         BoardData boardData = boardService.findById(id);
 
@@ -44,7 +44,7 @@ public class BoardApiController {
     }
 
     // {id}값이 들어오면 deleteBoard()로 넘어가 해당 id값의 글을 삭제 요청한다.
-    @DeleteMapping("/api/boards/{id}")
+    @DeleteMapping("/api/board/{id}")
     public ResponseEntity<Void> deleteBoard(@PathVariable Long id) {
         boardService.delete(id);
 
@@ -52,7 +52,7 @@ public class BoardApiController {
     }
 
     //PUT 요청시 {id}으로 요청이 들어오면 Request Body 정보가 request로 넘어가 응답 값을 body 담아 반환한다.
-    @PutMapping("/api/boards/{id}")
+    @PutMapping("/api/board/{id}")
     public ResponseEntity<BoardData> updateBoard(@PathVariable Long id, @RequestBody UpdateBoardDataRequest request) {
         BoardData updateBoardData = boardService.update(id, request);
 
