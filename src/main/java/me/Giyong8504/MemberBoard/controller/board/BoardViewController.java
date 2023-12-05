7c00ab8c7f4ac2085fe701db053d8ee3184511fa
Giyong8504 <kky5163@naver.com>
2023-12-05 21:41:39 +0900

    게시글 목록, 게시글 내용 뷰 구현(디자인은 나중에 작업 예정)

--- src/main/java/me/Giyong8504/MemberBoard/controller/board/BoardViewController.java
+++ src/main/java/me/Giyong8504/MemberBoard/controller/board/BoardViewController.java
@@ -2,10 +2,13 @@ package me.Giyong8504.MemberBoard.controller.board;
 
 import lombok.RequiredArgsConstructor;
 import me.Giyong8504.MemberBoard.dto.view.BoardListViewResponse;
+import me.Giyong8504.MemberBoard.dto.view.BoardViewResponse;
+import me.Giyong8504.MemberBoard.entities.BoardData;
 import me.Giyong8504.MemberBoard.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -15,6 +18,7 @@ public class BoardViewController {
 
     private final BoardService boardService;
 
+    // 게시글 목록
     @GetMapping("/board")
     public String getBoard(Model model){
         List<BoardListViewResponse> board = boardService.findAll()
@@ -22,5 +26,14 @@ public class BoardViewController {
         model.addAttribute("board", board);
 
         return "board/boardList";
+    }
+
+    // 게시글 내용
+    @GetMapping("/board/{id}")
+    public String getBoard(@PathVariable Long id, Model model) {
+        BoardData boardData = boardService.findById(id);
+        model.addAttribute("board", new BoardViewResponse(boardData));
+
+        return "board/boardContent";
     }
 }
