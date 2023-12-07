e3171cd0320ef163b3b702ae93e045b4624cd26e
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2023-12-07 16:21:53 +0900

    PropertyValueException 해결 및 글 등록 기능 추가 author 변수 nullable 에서 발생한 문제로 html에 작성란, js에 body에 author 보낼 내용 추가

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -44,3 +44,31 @@ if (modifyButton) {
         });
     });
 }
+
+
+// 등록 기능
+// id가 create-btn인 엘리먼트를 조회.
+const createButton = document.getElementById('create-btn');
+
+if (createButton) {
+    createButton.addEventListener('click', event => {
+        // 등록 API로 POST 요청을 보낸다.
+        fetch(`/api/board`, {
+            method: 'POST',
+            headers: { // 요청을 보낼 때 headers의 요청 형식을 지정한다.
+                "Content-Type": "application/json",
+            },
+
+            // body에 HTML에서 입력한 데이터를 JSON 형식으로 바꿔 보낸다.
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value,
+                author: document.getElementById('author').value
+            })
+        })
+        .then(() => {
+            alert('등록 되었습니다.');
+            location.replace('/board');
+        });
+    });
+}
