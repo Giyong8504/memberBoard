4a15e37f50bd454bfac1d12b5229eff4ef13c190
Giyong8504 <kky5163@naver.com>
2023-12-11 00:52:03 +0900

    타임리프에 메시지 사용을 위한 form 생성, validation.properties 추가

---
+++ src/main/java/me/Giyong8504/MemberBoard/controller/JoinForm.java
@@ -0,0 +1,35 @@
+package me.Giyong8504.MemberBoard.controller;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class JoinForm {
+
+    @NotBlank
+    @Size(min = 6)
+    private String userId;
+
+    @NotBlank
+    @Size(min = 8)
+    private String userPw;
+
+    @NotBlank
+    private String userPwRe;
+
+    @NotBlank
+    private String userNm;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    private String mobile;
+
+    @AssertTrue
+    private boolean agree;
+}
+
