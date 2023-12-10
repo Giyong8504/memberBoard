0f544cc138fed02ca2b2107a3a6560dae26ef2a0
Giyong8504 <kky5163@naver.com>
2023-12-11 00:54:11 +0900

    쿼리메서드 생성된 QUser를 가져와 사용. exists()

---
+++ src/main/java/me/Giyong8504/MemberBoard/controller/JoinValidator.java
@@ -0,0 +1,62 @@
+package me.Giyong8504.MemberBoard.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.Giyong8504.MemberBoard.repositories.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class JoinValidator implements Validator {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(JoinForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        JoinForm form = (JoinForm)target;
+
+
+        /**
+         * 1. 아이디 중복 체크 - 중복 X
+         * 2. 비밀번호, 비밀번호 확인 일치 여부
+         * 3. 휴대전화번호 형식 체크
+         */
+
+        //1 아이디 중복체크 - 중복 X
+        String userId = form.getUserId();
+        if (userId != null && !userId.isBlank() && userRepository.exists(userId)) {
+            errors.rejectValue("userId", "duplicate");
+        }
+
+        // 2. 비밀번호, 비밀번호 확인 일치 여부
+        String userPw = form.getUserPw();
+        String userPwRe = form.getUserPwRe();
+        if (!userPw.equals(userPwRe) && userPw != null && !userPw.isBlank()
+                && userPwRe != null && !userPwRe.isBlank()) {
+            errors.rejectValue("userPwRe", "mismatch");
+        }
+
+        // 3. 휴대전화번호 형식 체크
+        String mobile = form.getMobile();
+        if (mobile != null && !mobile.isBlank()) {
+            /**
+             * 010-1111-1111, 010.1111.1111 01011111111 -> 형식을 숫자만 남기고 제거
+             * 011, 016
+             */
+
+            mobile = mobile.replaceAll("\\D", "");
+            String pattern = "^01[016]\\d{3,4}\\d{4}$";
+            if (!mobile.matches(pattern)) {
+                errors.rejectValue("mobile", "Mobile");
+            }
+
+            form.setMobile(mobile);
+        }
+    }
+}
--- src/main/java/me/Giyong8504/MemberBoard/repositories/UserRepository.java
+++ src/main/java/me/Giyong8504/MemberBoard/repositories/UserRepository.java
@@ -1,10 +1,17 @@
 package me.Giyong8504.MemberBoard.repositories;
 
+import me.Giyong8504.MemberBoard.entities.QUser;
 import me.Giyong8504.MemberBoard.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User> {
     User findByUserId(String id); // id로 사용자 정보를 가져온다.
 
+    default boolean exists(String userId) {
+        QUser user = QUser.user;
+
+        return exists(user.userId.eq(userId));
+    }
 }
