70f3c8be7046a55c958b6cdd7e9a94c96dfd3abe
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2023-12-11 12:58:24 +0900

    user 컨트롤러 작성

---
+++ src/main/java/me/Giyong8504/MemberBoard/controller/UserController.java
@@ -0,0 +1,39 @@
+package me.Giyong8504.MemberBoard.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.Giyong8504.MemberBoard.models.user.UserJoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserJoinService userJoinService;
+
+    @GetMapping("/join") // 모델에서 사용할 속성 이름을 갑으로 설정
+    public String join(@ModelAttribute JoinForm joinForm) {
+
+        return "user/join";
+    }
+
+    @PostMapping("/join") // 검증 실패시 상태코드 응답
+    public String joinPs(@Valid JoinForm joinForm, Errors errors) {
+        userJoinService.join(joinForm, errors);
+
+        if (errors.hasErrors()) {
+            return "user/login";
+        }
+        return "redirect:user/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "user/login";
+    }
+}
