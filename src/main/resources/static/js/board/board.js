0260b7a16ada1117e9077785493517f4350b0eaf
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2023-12-27 15:16:38 +0900

    등록기능 수정, 쿠키 가져오기, HTTP 요청 보내는 함수 작성

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -74,24 +74,85 @@ if (createButton) {
 
     const isConfirmed = confirm('등록하시겠습니까?')
         if (isConfirmed) {
-            // 등록 API로 POST 요청을 보낸다.
-            fetch(`/api/board`, {
-                method: 'POST',
-                headers: { // 요청을 보낼 때 headers의 요청 형식을 지정한다.
-                    "Content-Type": "application/json",
-                },
-
-                // body에 HTML에서 입력한 데이터를 JSON 형식으로 바꿔 보낸다.
-                body: JSON.stringify({
-                    title: document.getElementById('title').value,
-                    content: document.getElementById('content').value,
-                    author: document.getElementById('author').value
-                })
-            })
-            .then(() => {
-                alert('등록 되었습니다.');
-                location.replace('/board');
+            // body에 HTML에서 입력한 데이터를 JSON 형식으로 바꿔 보낸다.
+            body = JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value,
+                author: document.getElementById('author').value
             });
+            function success() {
+                alert('등록 되었습니다');
+                location.replace('/board');
+            };
+
+            function fail() {
+                alert('등록 실패했습니다');
+                location.replace('/board');
+            };
+
+            httpRequest('POST','/api/board', body, success, fail)
+        };
+    });
+}
+
+
+// 쿠키를 가져오는 함수
+function getCookie(key) {
+    var result = null;
+    var cookie = document.cookie.split(';'); // ; 를 기준으로 나눠 배열로 저장
+    cookie.some(function (item) {
+        item = item.replace(' ', ''); // 각 쿠키 문자열에서 공백 제거
+
+        var dic = item.split('='); // 문자열을 '=' 기준으로 나눠 dic 배열에 저장
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+
+    return result;
+}
+
+// HTTP 요청을 보내는 함수
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, { // 지정된 URL에 HTTP 요청
+        method: method,
+        headers: { // 로컬 스토리지에서 액세스 토큰 값을 가져와 헤더에 추가
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+            'Content-Type': 'application/json',
+        },
+        body: body,
+    }).then(response => {
+        if (response.status === 200 || response.status === 201) {
+            return success();
+        }
+        const refresh_token = getCookie('refresh_token');
+        if (response.status === 401 && refresh_token) { // 토큰 갱신
+            fetch('/api/token', { // 새로운 엑세스 토큰 요청
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    refreshToken: getCookie('refresh_token'),
+                }),
+            })
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                })
+                .then(result => { // 재발급이 성공하면 로컬 스토리지값을 새로운 액세스 토큰으로 교체
+                    localStorage.setItem('access_token', result.accessToken);
+                    httpRequest(method, url, body, success, fail);
+                })
+                .catch(error => fail());
+        } else {
+            return fail();
         }
     });
 }
+
+
