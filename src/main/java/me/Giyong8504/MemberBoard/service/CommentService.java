e7b7b733238281d7162257b589b6007ddcb58e53
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-01-03 17:50:26 +0900

    댓글서비스 수정

--- src/main/java/me/Giyong8504/MemberBoard/service/CommentService.java
+++ src/main/java/me/Giyong8504/MemberBoard/service/CommentService.java
@@ -1,9 +1,9 @@
 package me.Giyong8504.MemberBoard.service;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.Giyong8504.MemberBoard.dto.AddCommentRequest;
-import me.Giyong8504.MemberBoard.dto.UpdateCommentRequest;
 import me.Giyong8504.MemberBoard.entities.BoardData;
 import me.Giyong8504.MemberBoard.entities.Comment;
 import me.Giyong8504.MemberBoard.repositories.BoardDataRepository;
@@ -58,7 +58,7 @@ public class CommentService {
 
     // 수정 기능
     @Transactional
-    public Comment updateComment(Long commentId, UpdateCommentRequest request) {
+    public Comment updateComment(Long commentId, @Valid AddCommentRequest request) {
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("not found" + commentId));
 
