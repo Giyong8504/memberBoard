f6fe5fc9211328b39448617dc96626589e54057d
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-01-10 15:59:46 +0900

    템플릿 이메일 속설 전달 기능 추가, 템플릿 html 생성, 테스트

--- src/main/java/me/Giyong8504/MemberBoard/email/EmailSendService.java
+++ src/main/java/me/Giyong8504/MemberBoard/email/EmailSendService.java
@@ -6,21 +6,46 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class EmailSendService {
     private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
 
-    public boolean sendMail(EmailMessage message) {
+    public boolean sendMail(EmailMessage message, String tpl, Map<String, Object> tplData) {
+        String text = null;
+
+        /* 이메일 템플릿을 사용하는 경우 EmailMessage의 제목, 내용, 수신인 및 tplData 추가 치환 속성을전달하고
+        타임리프로 번역된 텍스트를 반환값으로 처리 */
+        if (StringUtils.hasText(tpl)) {
+            tplData = Objects.requireNonNullElse(tplData, new HashMap<>());
+            Context context = new Context(); //thymeleaf
+
+            tplData.put("to", message.to());
+            tplData.put("subject", message.subject());
+            tplData.put("message", message.message());
+
+            context.setVariables(tplData);
+
+            text = templateEngine.process("email/" + tpl, context);
+        } else { // 템플릿 전송이 아닐 경우 메시지로 대체
+            text = message.message();
+        }
 
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false,"UTF-8");
             mimeMessageHelper.setTo(message.to()); // 메일 수신자
             mimeMessageHelper.setSubject(message.subject()); // 메일 제목
-            mimeMessageHelper.setText(message.message(), true); // 메일 내용
+            mimeMessageHelper.setText(text, true); // 메일 내용
             javaMailSender.send(mimeMessage); // 메일 전송
 
             return true;
@@ -30,5 +55,9 @@ public class EmailSendService {
         }
 
         return false;
+    }
+
+    public boolean sendMail(EmailMessage message) {
+        return sendMail(message, null, null);
     }
 }
--- src/test/java/me/Giyong8504/MemberBoard/email/EmailSendTest.java
+++ src/test/java/me/Giyong8504/MemberBoard/email/EmailSendTest.java
@@ -1,8 +1,12 @@
 package me.Giyong8504.MemberBoard.email;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 public class EmailSendTest {
@@ -15,9 +19,20 @@ public class EmailSendTest {
         EmailMessage message = new EmailMessage("kky5163@naver.com", "제목...", "내용...");
         boolean success = emailSendService.sendMail(message);
 
-        assertTure(success);
+        assertTrue(success);
     }
 
-    private void assertTure(boolean success) {
+    @Test
+    @DisplayName("템플릿 형태 전송 테스트")
+    void sendWithTplTest() {
+        EmailMessage message = new EmailMessage("kky5163@naver.com", "제목!!", "내용!!");
+        Map<String, Object> tplData = new HashMap<>();
+        tplData.put("authNum", "123456");
+        boolean success = emailSendService.sendMail(message, "auth", tplData);
+
+        assertTrue(success);
+    }
+
+    private void assertTrue(boolean success) {
     }
 }
