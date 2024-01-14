80d10ae11c340a366fec685053511fae8079a5e5
Giyong8504 <kky5163@naver.com>
2024-01-14 22:24:10 +0900

    이메일 인증 코드, 일치여부, 중복 여부 api 구현

--- src/main/java/me/Giyong8504/MemberBoard/email/controller/EmailApiController.java
+++ src/main/java/me/Giyong8504/MemberBoard/email/controller/EmailApiController.java
@@ -1,8 +1,8 @@
 package me.Giyong8504.MemberBoard.email.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.Giyong8504.MemberBoard.commons.rests.JSONData;
 import me.Giyong8504.MemberBoard.email.service.EmailVerifyService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +13,25 @@ public class EmailApiController {
 
     private final EmailVerifyService verifyService;
 
-    // 이메일 인증 코드 발급
+    // 이메일 인증코드 발급
     @GetMapping("/api/email/verify")
-    public ResponseEntity<Object> sendVerifyEmail(@RequestParam("email") String email) {
-        verifyService.sendCode(email);
+    public JSONData<Object> sendVerifyEmail(@RequestParam("email") String email) {
+        JSONData<Object> data = new JSONData<>();
 
-        return ResponseEntity.ok().build();
+        boolean result = verifyService.sendCode(email);
+        data.setSuccess(result);
+
+        return data;
     }
 
     // 발급받은 인증코드와 사용자 입력 코드의 일치 여부 체크
     @GetMapping("/api/email/auth_check")
-    public ResponseEntity<Object> checkVerifiedEmail(@RequestParam("authNum") int authNum) {
-        verifyService.check(authNum);
+    public JSONData<Object> checkVerifiedEmail(@RequestParam("authNum") int authNum) {
+        JSONData<Object> data = new JSONData<>();
 
-        return ResponseEntity.ok().build();
+        boolean result = verifyService.check(authNum);
+        data.setSuccess(result);
+
+        return data;
     }
 }
