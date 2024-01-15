b58282abb47304938b546f975ccf2ab05d9291e7
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-01-15 15:16:20 +0900

    이메일과 회원명 조합 조회 체크

---
+++ src/main/java/me/Giyong8504/MemberBoard/controller/user/FindPwValidation.java
@@ -0,0 +1,35 @@
+package me.Giyong8504.MemberBoard.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import me.Giyong8504.MemberBoard.dto.FindPwRequest;
+import me.Giyong8504.MemberBoard.repositories.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class FindPwValidation implements Validator {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(FindPwRequest.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        //이메일과 회원명 조합으로 조회되는지 체크
+        FindPwRequest form = (FindPwRequest) target;
+        String email = form.email();
+        String name = form.name();
+
+        if (StringUtils.hasText(email) && StringUtils.hasText(name)
+                && !userRepository.existsByEmailAndName(email, name)) {
+            errors.reject("NotFound.user");
+        }
+    }
+}
