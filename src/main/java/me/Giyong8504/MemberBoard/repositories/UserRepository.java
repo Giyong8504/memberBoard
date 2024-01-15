cebcd4f4a1138226a5721046dc21a22cf0d6aa35
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-01-15 14:59:25 +0900

    이메일과 회원명으로 조회 체크 querydsl 작성

--- src/main/java/me/Giyong8504/MemberBoard/repositories/UserRepository.java
+++ src/main/java/me/Giyong8504/MemberBoard/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package me.Giyong8504.MemberBoard.repositories;
 
+import com.querydsl.core.BooleanBuilder;
 import me.Giyong8504.MemberBoard.entities.QUser;
 import me.Giyong8504.MemberBoard.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +13,17 @@ public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredi
 
     Optional<User> findByEmail(String email); // 소셜 로그인 후 반환되는 값중 email을 확인.
 
-    default boolean exists(String email) {
+    default boolean exists(String email) { // 이메일이 존재하는지 확인.
         return exists(QUser.user.email.eq(email));
+    }
+
+    // 이메일과 회원명으로 조회되는지 체크
+    default boolean existsByEmailAndName(String email, String name) {
+        QUser user = QUser.user;
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(user.email.eq(email))
+                .and(user.userNm.eq(name));
+
+        return exists(builder);
     }
 }
