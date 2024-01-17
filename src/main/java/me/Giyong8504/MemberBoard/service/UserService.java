a5057d0bddcd00452ad440055f8f390f17a856ae
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-01-17 17:32:13 +0900

    현재 로그인한 사용자 정보 가져오는 메서드 추가

--- src/main/java/me/Giyong8504/MemberBoard/service/UserService.java
+++ src/main/java/me/Giyong8504/MemberBoard/service/UserService.java
@@ -1,10 +1,10 @@
 package me.Giyong8504.MemberBoard.service;
 
 import lombok.RequiredArgsConstructor;
-import me.Giyong8504.MemberBoard.dto.AddUserRequest;
 import me.Giyong8504.MemberBoard.entities.User;
 import me.Giyong8504.MemberBoard.repositories.UserRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,5 +21,14 @@ public class UserService {
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(()  -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    // 현재 로그인한 사용자 정보 가져오기
+    public User getLoggedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        return (User) authentication.getPrincipal();
     }
 }
