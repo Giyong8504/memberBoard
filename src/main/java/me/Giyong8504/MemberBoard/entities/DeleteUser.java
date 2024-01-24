a6f0755149e018894fc3be12f5e738a8fa1178d7
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-01-24 16:16:03 +0900

    탈퇴한 회원 정보를 담을 엔터티, 리포지토리 추가

---
+++ src/main/java/me/Giyong8504/MemberBoard/entities/DeleteUser.java
@@ -0,0 +1,30 @@
+package me.Giyong8504.MemberBoard.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.Giyong8504.MemberBoard.commons.Role;
+
+@Table(name = "delete_user")
+@Entity
+@NoArgsConstructor
+@Data
+public class DeleteUser extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private String id;
+
+    @Column
+    private String Email;
+
+    @Column
+    private String userNm;
+
+    @Column
+    private String mobile;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+}
---
+++ src/main/java/me/Giyong8504/MemberBoard/repositories/DeleteUserRepository.java
@@ -0,0 +1,8 @@
+package me.Giyong8504.MemberBoard.repositories;
+
+import me.Giyong8504.MemberBoard.entities.DeleteUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeleteUserRepository extends JpaRepository<DeleteUser, Long> {
+
+}
