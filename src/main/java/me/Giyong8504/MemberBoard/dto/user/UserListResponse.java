326e0c7758d9705d2f95e45809c9c637f0e83b61
Giyong8504 <kky5163@naver.com>
2024-01-28 23:11:00 +0900

    회원 정보 dto 작성

---
+++ src/main/java/me/Giyong8504/MemberBoard/dto/user/UserListResponse.java
@@ -0,0 +1,26 @@
+package me.Giyong8504.MemberBoard.dto.user;
+
+import lombok.Getter;
+import me.Giyong8504.MemberBoard.entities.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserListResponse {
+
+    private Long id;
+    private String email;
+    private String userNm;
+    private String mobile;
+    private LocalDateTime reg_dt;
+    private LocalDateTime mod_dt;
+
+    public UserListResponse(User user) {
+        this.id = user.getUserNo();
+        this.email = user.getEmail();
+        this.userNm = user.getUserNm();
+        this.mobile = user.getMobile();
+        this.reg_dt = user.getRegDt();
+        this.mod_dt = user.getModDt();
+    }
+}
