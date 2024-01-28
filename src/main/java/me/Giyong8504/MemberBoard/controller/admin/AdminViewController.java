f367ece3ddcd0b28def66cce81d865fc8ce90d9c
Giyong8504 <kky5163@naver.com>
2024-01-29 00:53:23 +0900

    사용자 권한 변경 POST 매핑 추가

--- src/main/java/me/Giyong8504/MemberBoard/controller/admin/AdminViewController.java
+++ src/main/java/me/Giyong8504/MemberBoard/controller/admin/AdminViewController.java
@@ -1,14 +1,19 @@
 package me.Giyong8504.MemberBoard.controller.admin;
 
 import lombok.RequiredArgsConstructor;
+import me.Giyong8504.MemberBoard.commons.Role;
 import me.Giyong8504.MemberBoard.dto.user.UserListResponse;
 import me.Giyong8504.MemberBoard.dto.boards.BoardListViewResponse;
+import me.Giyong8504.MemberBoard.entities.User;
+import me.Giyong8504.MemberBoard.repositories.UserRepository;
 import me.Giyong8504.MemberBoard.service.BoardService;
 import me.Giyong8504.MemberBoard.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,6 +24,7 @@ public class AdminViewController {
 
     private final BoardService boardService;
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping("/index")
     public String adIndex() {
@@ -45,5 +51,16 @@ public class AdminViewController {
         model.addAttribute("adminUserList", userList);
 
         return "admin/user/userList";
+    }
+
+    // 사용자 권한 변경
+    @PostMapping("/changeUserRole")
+    public String changeUserRole(@RequestParam Long userId, @RequestParam Role newRole) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            user.setRole(newRole);
+            userRepository.save(user);
+        }
+        return "redirect:/admin/userList";
     }
 }
