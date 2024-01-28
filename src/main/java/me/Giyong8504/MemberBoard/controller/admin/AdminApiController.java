b3b9374f8c504145db4cd6ec904ea788532cae26
Giyong8504 <kky5163@naver.com>
2024-01-29 02:34:49 +0900

    관리자 권한 탈퇴 api 작성 및 js 작성

--- src/main/java/me/Giyong8504/MemberBoard/controller/admin/AdminApiController.java
+++ src/main/java/me/Giyong8504/MemberBoard/controller/admin/AdminApiController.java
@@ -2,6 +2,7 @@ package me.Giyong8504.MemberBoard.controller.admin;
 
 import lombok.RequiredArgsConstructor;
 import me.Giyong8504.MemberBoard.service.AdminBoardListService;
+import me.Giyong8504.MemberBoard.service.AdminUserListService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,10 +13,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class AdminApiController {
 
     private final AdminBoardListService adminBoardListService;
+    private final AdminUserListService adminUserListService;
 
+    // 관리자 권한 회원 게시글 삭제
     @DeleteMapping("/api/userBoardList/{id}")
     public ResponseEntity<Void> deleteBoard(@PathVariable Long id) {
         adminBoardListService.delete(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // 관리자 권한 사용자 강제 탈퇴
+    @DeleteMapping("/api/userList/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        adminUserListService.userDelete(id);
 
         return ResponseEntity.ok().build();
     }
--- src/main/resources/static/js/admin/board.js
+++ src/main/resources/static/js/admin/board.js
@@ -27,3 +27,34 @@ if (adminDeleteButton) {
         });
     });
 }
+
+
+// 회원 강제 탈퇴 기능
+const userDeleteByAdminButton = document.querySelectorAll('.userDeleteByAdmin-btn');
+
+if (userDeleteByAdminButton) {
+    userDeleteByAdminButton.forEach(button => {
+        button.addEventListener('click', event => {
+            // 사용자에게 확인을 받기 위한 경고창 표시
+            const isConfirmed = confirm('삭제 하시겠습니까?');
+            if (isConfirmed) {
+                let params = new URLSearchParams(location.search);
+                let id = button.getAttribute('data-user-id');
+
+                fetch(`/api/userList/${id}`, {
+                    method: 'DELETE'
+                })
+                .then(response => {
+                    if (response.ok) {
+                        // 서버에서 삭제가 성공적으로 이루어진 경우
+                        alert('삭제 되었습니다.');
+                        location.replace('/admin/userList');
+                    } else {
+                        // 서버에서 삭제가 실패한 경우 또는 권한이 없는 경우
+                        alert('삭제 권한이 없습니다.');
+                    }
+                })
+            }
+        });
+    });
+}
