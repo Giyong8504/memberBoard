a0d7a1cf1c0fbeee8aa6bcd61139ae15dc1930fa
Giyong8504 <128211712+Giyong8504@users.noreply.github.com>
2024-02-01 15:05:35 +0900

    경로 접근 권한 설정 변경 OAuth 로그인 사용자는 Role 권한으로 접속 가능한 페이지에 접근이 안되어 변경함.

--- src/main/java/me/Giyong8504/MemberBoard/configs/WebSecurityConfig.java
+++ src/main/java/me/Giyong8504/MemberBoard/configs/WebSecurityConfig.java
@@ -59,7 +59,8 @@ public class WebSecurityConfig {
         // 토큰 재발급, 이메일 URL은 인증없이 접근 가능하도록 설정. 나머지 API URL은 인증 필요
         http.authorizeHttpRequests(c -> {
             c.requestMatchers("/admin/**").hasAuthority("ADMIN") // "/admin/" 경로 요청은 'ADMIN' 권한을 가진 사용자만 접근 가능
-                    .requestMatchers("/myPage/**", "/new-board", "/changePassword/**", "/deleteId/**").hasAnyAuthority("USER", "GOOGLE","ADMIN") // "/myPage/**","/new-board" 경로 요청은 인증된 사용자만 접근 가능
+                    //.requestMatchers("/myPage/**", "/new-board", "/changePassword/**", "/deleteId/**").hasAnyAuthority("USER", "GOOGLE","ADMIN") // OAuth 로그인 사용자는 접근이 안되므로 보류.
+                    .requestMatchers("/myPage/**", "/new-board", "/changePassword/**", "/deleteId/**").authenticated() // 해당 경로 요청은 인증된 사용자만 접근 가능
                     .requestMatchers("/api/token", "/api/email/**").permitAll()
                     .anyRequest().permitAll(); // 그 외 모든 요청은 누구나 접근 가능
         });
